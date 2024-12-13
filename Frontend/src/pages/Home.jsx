f37d90a059a797eacb30fbcfa055c3a5b16cf08a
Naman Dadhich <namandadhich15592@gmail.com>
2024-12-13 20:59:18 +0530

    styling to display

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Header from '../components/Header'
 import ExploreMenu from '../components/ExploreMenu'
+import FoodDisplay from '../components/FoodDisplay';
 
 function Home() {
 
@@ -10,6 +11,7 @@ function Home() {
     <div>
         <Header/>
         <ExploreMenu category={category} setCategory={setCategory}/>
+        <FoodDisplay category={ category } />
     </div>
   )
 }
