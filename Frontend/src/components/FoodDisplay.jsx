6d64d8243f40130b179ee0c3522be3154e9a376f
Naman Dadhich <namandadhich15592@gmail.com>
2025-01-12 23:48:11 +0530

    progress

--- Frontend/src/components/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay.jsx
@@ -1,19 +1,33 @@
-import React, { useContext } from 'react'
-import { StoreContext } from '../context/StoreContext'
-import FoodItem from './FoodItem'
+import React, { useContext } from "react";
+import { StoreContext } from "../context/StoreContext";
+import FoodItem from "./FoodItem";
 
-function FoodDisplay({category}) {
-    const { food_list } = useContext(StoreContext)
+function FoodDisplay({ category }) {
+  const { food_list } = useContext(StoreContext);
   return (
-    <div className='mt-8' id='food-display'>
-        <h2 className='text-[max(2vw,24px)] font-semibold'>Top Dishes Near You</h2>
-        <div className="grid grid-cols-[repeat(auto-fill,_minmax(240px,1fr))] mt-8 gap-8 gap-y-12">
-            {food_list.map((item, index) => {
-                return <FoodItem key={index} id={item._id} name={item.name} rate={item.rate} description={item.description} price={item.price} image={item.image} />
-            })}
-        </div>
+    <div className="mt-8" id="food-display">
+      <h2 className="text-[max(2vw,24px)] font-semibold">
+        Top Dishes Near You
+      </h2>
+      <div className="grid grid-cols-[repeat(auto-fill,_minmax(240px,1fr))] mt-8 gap-8 gap-y-12">
+        {food_list.map((item, index) => {
+          if ((category === "All" || category === item.category)) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                rate={item.rate}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default FoodDisplay
+export default FoodDisplay;
